440da4a4be56e4d68eaca2e7321e9b1b89e320d1
George <george.ursachi7@gmail.com>
2020-05-03 14:25:41 +0200

    Added cars table

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,11 +1,18 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.Car;
+import com.example.demo.Service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    @Autowired
+    CarService carService;
 
     @GetMapping("/")
     public String index() {
@@ -15,7 +22,8 @@ public class HomeController {
 
     @GetMapping("/carMenu")
     public String carMenu(Model model) {
-
+        List<Car> carList = carService.readAll();
+        model.addAttribute("cars",carList);
         return "home/carMenu";
     }
 }
--- src/main/java/com/example/demo/Repository/CarRepo.java
+++ src/main/java/com/example/demo/Repository/CarRepo.java
@@ -2,7 +2,9 @@ package com.example.demo.Repository;
 
 import com.example.demo.Model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +15,11 @@ public class CarRepo {
     JdbcTemplate template;
 
     public List<Car> readAll() {
-        return null;
+        String sqlQuery = "SELECT car.car_id, car.reg_nr, car.reg_date, car.odometer, car.model_id, model.model_group, model.brand, model.model_details, model.fuel_type \n" +
+                "FROM car\n" +
+                "INNER JOIN model ON car.model_id = model.model_id\n" +
+                "ORDER BY car_id";
+        RowMapper<Car> rowMapper = new BeanPropertyRowMapper<>(Car.class);
+        return template.query(sqlQuery, rowMapper);
     }
 }
